b7cc95012438654aab3bdec0ef35def63de19927
Irfan Rafif <mirfanrafif17@gmail.com>
2019-11-02 21:07:49 +0700

    add welcome page

--- templates/navbar.php
+++ templates/navbar.php
@@ -3,40 +3,55 @@ $dir = $_SERVER['DOCUMENT_ROOT'];
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <!-- navbar toggler -->
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
+  <div class="container">
+    <a class="navbar-brand" href="#">Kosku</a>
+    <!-- navbar toggler -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <!-- navbar -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- kiri -->
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="">Pengguna</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pembayaran</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Laporan</a>
+        </li>
+      </ul>
 
-      <!-- navbar -->
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- kiri -->
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href=""
-              >Pengguna</a
-            >
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pembayaran</a>
-          </li>
-        </ul>
-
-        <!-- kanan -->
-        <form class="form-inline my-2 my-lg-0">
-          <button class="btn btn-danger" type="submit">
-            Log Out
-          </button>
-        </form>
+      <!-- kanan -->
+      <div class="dropdown">
+        <button
+          class="btn btn-warning dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          Irfan Rafif
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#">Profile</a>
+          <a class="dropdown-item" href="#">Settings</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Logout</a>
+        </div>
       </div>
-    </nav>
+    </div>
+  </div>
+</nav>
--- welcome.php
+++ welcome.php
@@ -1,19 +1,33 @@
 <html>
   <head>
     <style>
-      .grid {
-        display: grid;
-        grid-template-columns: auto auto auto;
-      }
-
-      .grid-item {
-        display: inline-grid;
-      }
-
-      .head {
+      #header {
         height: 500px;
         background-image: url("assets/head-bg.jpg");
         background-size: cover;
+        padding-top: 200px;
+      }
+
+      .benefit {
+        padding-top: 100px;
+      }
+
+      .img-benefit {
+        width: 500px;
+        height: auto;
+      }
+
+      .circle {
+        width: 200px;
+        height: 200px;
+        border: 10px solid #ffbe15;
+        border-radius: 50%;
+      }
+
+      #footer {
+        margin-top: 50px;
+        height: 200px;
+        padding-top: 25px;
       }
     </style>
     <?php 
@@ -25,24 +39,126 @@
     include("templates/navbar.php")
     ?>
 
-    <div class="container">
-      <div class="head">
-        
+    <div class="container" id="header">
+      <div style="padding-left: 100px; padding-right: 100px;">
+        <h1 class="display-4 text-light text-center">
+          Manajemen Kos Dalam 1 Aplikasi
+        </h1>
+        <h6 class="text-light text-center">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          distinctio pariatur perferendis delectus dolore dolores quae quos non
+          corporis libero eligendi nobis, atque totam. Officia voluptate nam
+          maiores sint maxime.
+        </h6>
       </div>
+    </div>
+    <div class="container benefit">
       <div class="row">
-        <div class="col-sm">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
-          incidunt, tempora quasi dignissimos ullam adipisci possimus sunt natus
-          dolores aperiam consequatur ratione quam praesentium culpa, sint
-          perferendis officiis eum omnis!
+        <div class="col-lg">
+          <img src="assets/undraw_file_manager_j85s.svg" class="img-benefit" />
         </div>
-        <div class="col-sm">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
-          incidunt, tempora quasi dignissimos ullam adipisci possimus sunt natus
-          dolores aperiam consequatur ratione quam praesentium culpa, sint
-          perferendis officiis eum omnis!
+        <div class="col-lg">
+          <h3 style="font-weight: 500;">Kesusahan dalam manajemen kos?</h3>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
+            incidunt, tempora quasi dignissimos ullam adipisci possimus sunt
+            natus dolores aperiam consequatur ratione quam praesentium culpa,
+            sint perferendis officiis eum omnis!
+          </p>
         </div>
-        <!-- <div class="col-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe incidunt, tempora quasi dignissimos ullam adipisci possimus sunt natus dolores aperiam consequatur ratione quam praesentium culpa, sint perferendis officiis eum omnis!</div> -->
+      </div>
+    </div>
+    <div class="container benefit">
+      <div class="row">
+        <div class="col-lg">
+          <h3 style="font-weight: 500;">Kesusahan dalam manajemen kos?</h3>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
+            incidunt, tempora quasi dignissimos ullam adipisci possimus sunt
+            natus dolores aperiam consequatur ratione quam praesentium culpa,
+            sint perferendis officiis eum omnis!
+          </p>
+        </div>
+        <div class="col-lg">
+          <img src="assets/undraw_file_manager_j85s.svg" class="img-benefit" />
+        </div>
+      </div>
+    </div>
+    <div class="container benefit">
+      <div class="row">
+        <div class="col-lg">
+          <div class="card">
+            <div
+              class="text-center"
+              style="padding-top: 50px; padding-bottom: 30px;"
+            >
+              <div class="display-1">1</div>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg">
+          <div class="card">
+            <div
+              class="text-center"
+              style="padding-top: 50px; padding-bottom: 30px;"
+            >
+              <div class="display-1">2</div>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg">
+          <div class="card">
+            <div
+              class="text-center"
+              style="padding-top: 50px; padding-bottom: 30px;"
+            >
+              <div class="display-1">3</div>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container benefit text-center">
+      <h2>Tertarik? Daftar Dulu Aja...</h2>
+      <a href="#" class="btn btn-primary">Daftar</a>
+    </div>
+    <div class="container bg-warning" id="footer">
+      <h5 class="text-center">Tim : <br /><br /></h5>
+      <div class="row">
+        <div class="col-lg"><h4>TI-2H</h4></div>
+        <div class="col-lg">
+          Moch. Irfan Rafif <br />
+          Bahrul Munir <br />
+          Wardah Ghaliyah <br />
+        </div>
+        <div class="col-lg">
+          15 <br />
+          27 <br />
+          7
+        </div>
       </div>
     </div>
   </body>
