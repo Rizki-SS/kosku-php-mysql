53f1713e44e9f4f0e7fb91c1defc911e2dfdc30c
Irfan Rafif <mirfanrafif17@gmail.com>
2019-11-07 09:34:59 +0700

    redesign daftar and login

--- daftar.php
+++ daftar.php
@@ -1,80 +1,120 @@
 <html>
-  <head>
-    <?php 
-    $dir = $_SERVER['DOCUMENT_ROOT'];
-    include ($dir."/templates/resources.php");
-    $msg = "";
-    session_start();
-    if (isset($_SESSION["error"])) {
-      $msg = $_SESSION["error"];
+
+<head>
+  <?php
+  $dir = $_SERVER['DOCUMENT_ROOT'];
+  include($dir . "/templates/resources.php");
+  $msg = "";
+  if (isset($_GET["error"])) {
+    $error = $_GET["error"];
+  } else {
+    $error = "";
+  }
+
+  if ($error == "1") {
+    $msg = "Data sudah ada. Silahkan Login.";
+  } else if ($error == "2") {
+    $msg = "Data Tidak Valid";
+  }
+
+  ?>
+  <style>
+    .login {
+      width: 200px;
+      margin: auto;
+      padding-left: 100px;
+      padding-right: 100px;
     }
-    session_abort();
-    ?>
-    <style>
-      #login {
-        margin-top: 100px;
-      }
-    </style>
-  </head>
-  <body class="bg-dark">
-    <?php include ($dir."/templates/navbar.php");?>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-4 offset-4">
-          <div class="card text-center" id="login">
-            <div class="card-body">
-              <h4 class="card-title">Daftar</h4>
-              <br />
-              <?php 
-                if (isset($_SESSION["error"])) {
-                ?>
-              <div class="alert alert-danger alert-dismissible fade show">
-                <?= $msg ?>
-                <button
-                  type="button"
-                  class="close"
-                  data-dismiss="alert"
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <?php 
-                }
-                ?>
-              <form action="/daftar.php" class="form-group" method="POST">
-                <input type="text" name="name" class="form-control" /><br />
-                <input type="email" name="email" class="form-control" /><br />
-                <input type="password" name="password" class="form-control" />
-                <br />
-                <input
-                  type="submit"
-                  name="daftar"
-                  value="Daftar"
-                  class="btn btn-dark"
-                />
-              </form>
-            </div>
+
+    #hiasan {
+      background: url("assets/login-bg.jpg");
+      background-size: cover;
+      padding: 50px;
+      padding-top: 100px;
+    }
+
+    .row {
+      height: 100%;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-6" id="hiasan">
+        <h2 style="font-weight: bold;">
+          Kosku
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          sapiente vero ratione quisquam atque doloremque autem, reprehenderit
+          aliquam voluptates ut ullam praesentium voluptatibus dolorem ex cum
+          porro quaerat consequatur nam?
+        </p>
+        <br /><br />
+        <span>Sudah punya akun?</span><br />
+        <a href="/login.php" class="btn btn-dark">Login</a>
+      </div>
+      <div class="col-lg-6 login">
+
+        <h2 class="h2" style="font-weight: bold;">Daftar</h2>
+        <br />
+        <?php
+        if (!empty($msg)) {
+          ?>
+          <div class="alert alert-danger alert alert-danger alert-dismissible fade show">
+            <?= $msg ?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
           </div>
-        </div>
+        <?php
+        }
+        ?>
+        <form action="/daftar.php" class="form-group" method="POST">
+          <label for="nama">Nama</label>
+          <input type="text" name="name" class="form-control" id="name" placeholder="Nama" /><br />
+          <label for="email">Email</label>
+          <input type="email" name="email" class="form-control" id="email" placeholder="Email" /><br />
+          <label for="password">Password</label>
+          <input type="password" name="password" class="form-control" id="password" placeholder="Password" />
+          <br />
+          <input type="submit" name="daftar" value="Daftar" class="btn btn-dark" />
+        </form>
       </div>
     </div>
-  </body>
+  </div>
+</body>
+
 </html>
-<?php 
+<?php
 if (isset($_POST["daftar"])) {
-  include($dir."/config/conn.php");
-  
+  include($dir . "/config/conn.php");
+
   $name = $_POST["name"];
   $email = $_POST["email"];
   $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-  $insert = "INSERT INTO user values (NULL, '$name', '$email', '$password', NOW(), NOW());";
-  $res = mysqli_query($conn, $insert);
-  if (mysqli_error($conn)) {
-    echo "Error : " . mysqli_error($conn);
-  }else{
-    header("location: /welcome.php");
+  if (empty($name) || empty($email) || empty($password)) {
+    header("location: daftar.php?error=2");
+  } else {
+    $find = "SELECT * FROM user WHERE email='$email';";
+    echo $find;
+    $res = mysqli_query($conn, $find);
+    $data = mysqli_fetch_assoc($res);
+
+    if (!empty($data)) {
+      header("location: daftar.php?error=1");
+    } else {
+      $insert = "INSERT INTO user values (NULL, '$name', '$email', '$password', NOW(), NOW());";
+      $res = mysqli_query($conn, $insert);
+      if (mysqli_error($conn)) {
+        echo "Error : " . mysqli_error($conn);
+      } else {
+        header("location: /login.php");
+      }
+    }
   }
 }
 ?>
--- login.php
+++ login.php
@@ -19,50 +19,87 @@
     }
     ?>
     <style>
-      #login {
-        margin-top: 100px;
+      .login {
+        width: 200px;
+        margin: auto;
+        padding-left: 100px;
+        padding-right: 100px;
+      }
+
+      #hiasan {
+        background: url("assets/login-bg.jpg");
+        background-size: cover;
+        padding: 50px;
+        padding-top: 100px;
+      }
+
+      .row {
+        height: 100%;
       }
     </style>
   </head>
-  <body class="bg-dark">
-    <?php include ($dir."/templates/navbar.php");?>
+  <body>
     <div class="container-fluid">
       <div class="row">
-        <div class="col-sm-4 offset-4">
-          <div class="card text-center" id="login">
-            <div class="card-body">
-              <h4 class="card-title">Login</h4>
-              <br />
-                <?php 
+        <div class="col-lg-6" id="hiasan">
+          <h2 style="font-weight: bold;">
+            Kosku
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            sapiente vero ratione quisquam atque doloremque autem, reprehenderit
+            aliquam voluptates ut ullam praesentium voluptatibus dolorem ex cum
+            porro quaerat consequatur nam?
+          </p>
+          <br /><br />
+          <span>Belum punya akun?</span><br />
+          <a href="/daftar.php" class="btn btn-dark">Daftar</a>
+        </div>
+        <div class="col-lg-6 login">
+          <?php 
                 if (!empty($pesan)) {
                 ?>
-              <div class="alert alert-danger alert alert-danger alert-dismissible fade show">
-                <?= $pesan ?>
-                <button
-                  type="button"
-                  class="close"
-                  data-dismiss="alert"
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <?php 
+          <div
+            class="alert alert-danger alert alert-danger alert-dismissible fade show"
+          >
+            <?= $pesan ?>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="alert"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <?php 
                 }
                 ?>
-              <form action="/config/check.php" class="form-group" method="POST">
-                <input type="email" name="email" class="form-control" /><br />
-                <input type="password" name="password" class="form-control" />
-                <br />
-                <input
-                  type="submit"
-                  name="submit"
-                  value="submit"
-                  class="btn btn-dark"
-                />
-              </form>
-            </div>
-          </div>
+          <h2 class="h2" style="font-weight: bold;">Login</h2>
+          <br /><br />
+          <form action="/config/check.php" class="form-group" method="POST">
+            <label for="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              class="form-control"
+              id="email"
+            /><br />
+            <label for="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              class="form-control"
+              id="password"
+            />
+            <br />
+            <input
+              type="submit"
+              name="submit"
+              value="Login"
+              class="btn btn-dark"
+            />
+          </form>
         </div>
       </div>
     </div>
