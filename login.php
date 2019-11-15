a32a6bb68ae5d6a310b86ef3b270d3ca86bbd5b8
Irfan Rafif <mirfanrafif17@gmail.com>
2019-11-15 15:25:37 +0700

    formatting

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
 <html>
-  <head>
-    <title>Kosku - Login</title>
-    <?php
+
+<head>
+  <title>Kosku - Login</title>
+  <?php
   $dir = $_SERVER['DOCUMENT_ROOT'];
   include($dir . "/templates/resources.php");
 
@@ -18,90 +19,69 @@
     $pesan = "Password Invalid";
   }
   ?>
-    <style>
-      .login {
-        margin: auto;
-        padding-left: 100px;
-        padding-right: 100px;
-      }
+  <style>
+    .login {
+      margin: auto;
+      padding-left: 100px;
+      padding-right: 100px;
+    }
 
-      #hiasan {
-        background: url("assets/login-bg.jpg");
-        background-size: cover;
-        padding: 50px;
-        padding-top: 100px;
-      }
+    #hiasan {
+      background: url("assets/login-bg.jpg");
+      background-size: cover;
+      padding: 50px;
+      padding-top: 100px;
+    }
 
-      .row {
-        height: 100%;
-      }
-    </style>
-  </head>
+    .row {
+      height: 100%;
+    }
+  </style>
+</head>
 
-  <body>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-lg-6" id="hiasan">
-          <h2 style="font-weight: bold;">
-            Kosku
-          </h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            sapiente vero ratione quisquam atque doloremque autem, reprehenderit
-            aliquam voluptates ut ullam praesentium voluptatibus dolorem ex cum
-            porro quaerat consequatur nam?
-          </p>
-          <br /><br />
-          <span>Belum punya akun?</span><br />
-          <a href="/daftar.php" class="btn btn-dark">Daftar</a>
-        </div>
-        <div class="col-lg-6 login">
-          <?php
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-6" id="hiasan">
+        <h2 style="font-weight: bold;">
+          Kosku
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          sapiente vero ratione quisquam atque doloremque autem, reprehenderit
+          aliquam voluptates ut ullam praesentium voluptatibus dolorem ex cum
+          porro quaerat consequatur nam?
+        </p>
+        <br /><br />
+        <span>Belum punya akun?</span><br />
+        <a href="/daftar.php" class="btn btn-dark">Daftar</a>
+      </div>
+      <div class="col-lg-6 login">
+        <?php
         if (!empty($pesan)) {
           ?>
-          <div
-            class="alert alert-danger alert alert-danger alert-dismissible fade show"
-          >
+          <div class="alert alert-danger alert alert-danger alert-dismissible fade show">
             <?= $pesan ?>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="alert"
-              aria-label="Close"
-            >
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <?php
+        <?php
         }
         ?>
-          <h2 class="h2" style="font-weight: bold;">Login</h2>
-          <br /><br />
-          <form action="/config/check.php" class="form-group" method="POST">
-            <label for="username">Email</label>
-            <input
-              type="name"
-              name="username"
-              class="form-control"
-              id="username"
-            /><br />
-            <label for="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              class="form-control"
-              id="password"
-            />
-            <br />
-            <input
-              type="submit"
-              name="submit"
-              value="Login"
-              class="btn btn-dark"
-            />
-          </form>
-        </div>
+        <h2 class="h2" style="font-weight: bold;">Login</h2>
+        <br /><br />
+        <form action="/config/check.php" class="form-group" method="POST">
+          <label for="username">Email</label>
+          <input type="name" name="username" class="form-control" id="username" /><br />
+          <label for="password">Password</label>
+          <input type="password" name="password" class="form-control" id="password" />
+          <br />
+          <input type="submit" name="submit" value="Login" class="btn btn-dark" />
+        </form>
       </div>
     </div>
-  </body>
+  </div>
+</body>
+
 </html>
