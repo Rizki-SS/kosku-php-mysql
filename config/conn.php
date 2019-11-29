15f822e78594eb5b14564e13bdd361d0591c9d72
Irfan Rafif <mirfanrafif17@gmail.com>
2019-11-30 06:35:19 +0700

    re add conn

---
+++ config/conn.php
@@ -0,0 +1,8 @@
+<?php
+$db_host = "mysql-6367-0.cloudclusters.net";
+$db_username = "mirfanrafif";
+$db_password = "irfanRAFI";
+$db_name = "kosku";
+$db_port = 10015;
+
+$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name. $db_port);
