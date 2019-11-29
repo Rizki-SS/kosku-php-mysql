9cdd216ff02f95b7860adf3f58d7034b00a630bf
Irfan Rafif <mirfanrafif17@gmail.com>
2019-11-30 06:30:58 +0700

    clear cached

--- config/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_host = "mysql-6367-0.cloudclusters.net";
-$db_username = "mirfanrafif";
-$db_password = "irfanRAFI";
-$db_name = "kosku";
-
-$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name, 10015);
