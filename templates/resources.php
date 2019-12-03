bda1b1a6555cad4cc11d8665c57f65b29defa4c1
Irfan Rafif <mirfanrafif17@gmail.com>
2019-12-03 12:47:17 +0700

    ganti jadi bootstrap material

--- templates/resources.php
+++ templates/resources.php
@@ -2,7 +2,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+<link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+
 <script src="/templates/jquery/jquery-3.4.1.js"></script>
-<link rel="stylesheet" href="/templates/bootstrap/css/bootstrap.min.css" />
-<script src="/templates/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/66cb53a8d1.js" crossorigin="anonymous"></script>
+<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
